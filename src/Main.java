9ff83a7535c86378199707b0eab01b028389e181
exercio-java-poo <victorbaraldi@gmail.com>
2025-08-28 20:59:50 -0300

    Adicionando

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Produto p1 = new Produto("Mouse", 80.0, 10);
+        Produto p2 = new Produto("Teclado", 120.0, 0);
+        Produto p3 = new Produto("Cadeira", 500.0, 5);
+        Produto p4 = new Produto("Fone", 40.0, 20);
+
+        List<Produto> produtos = Arrays.asList(p1, p2, p3, p4);
+
+        Predicate<Produto> precoMaiorQue50 = p -> p.preco > 50;
+        Predicate<Produto> disponivel = p -> p.quantidade > 0;
+
+        Function<Produto, String>  formatarProduto = p -> {
+            double precoComDesconto = p.preco * 0.9;
+            return String.format(
+                    "Produto: %s - Preço com desconto: R$%.2f",
+                    p.nome, precoComDesconto);
+        } ;
+
+        produtos.stream()
+                .filter(precoMaiorQue50)
+                .filter(disponivel)
+                .map(formatarProduto)
+                .forEach(System.out::println);
+    }
+}
---
+++ src/Produto.java
@@ -0,0 +1,24 @@
+public class Produto {
+
+    final String nome;
+    final double preco;
+    final int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
